44ffa8269a9cbb9cccec8856f5cc19281b9e14cd
Sabuhi Huseynov <sabuhi.huseynov@guavapay.com>
2024-10-26 20:30:39 +0400

    isPositive code added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        boolean isPositive = a > 0;
+        System.out.println(isPositive);
     }
+
 }
